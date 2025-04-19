039cb4de343192b9949a24d2b11391e31786efc3
Aman Madhukar <amanmadhukar@MacBook-Air.local>
2025-04-20 00:26:36 +0530

    added toast messages for signup and login status

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,50 +1,62 @@
 import Link from "next/link";
 import ThemeSwitch from "./ThemeSwitch";
+import { Toaster } from "react-hot-toast";
 
 export default function Navbar() {
   return (
-    <div className="navbar shadow-sm fixed top-0">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+    <>
+      <div className="navbar shadow-sm fixed top-0">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+              <li><a>Item 1</a></li>
+              <li>
+                <a>Parent</a>
+                <ul className="p-2">
+                  <li><a>Submenu 1</a></li>
+                  <li><a>Submenu 2</a></li>
+                </ul>
+              </li>
+              <li><a>Item 3</a></li>
+            </ul>
           </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-            <li><a>Item 1</a></li>
+          <Link href={'/'} className="btn btn-ghost text-xl">chessUI</Link>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li><a>Hire Me</a></li>
             <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li><a>Submenu 1</a></li>
-                <li><a>Submenu 2</a></li>
-              </ul>
+              <details className="dropdown">
+                <summary>Mode</summary>
+                <ul className="menu dropdown-content bg-base-300 rounded-box z-1 w-52 p-2 shadow-sm">
+                  <li><a>2 Player</a></li>
+                  <li><a>vs Friend</a></li>
+                  <li><a>Dice Chess</a></li>
+                </ul>
+              </details>
             </li>
-            <li><a>Item 3</a></li>
+            <li><a>Developer</a></li>
           </ul>
         </div>
-        <Link href={'/'} className="btn btn-ghost text-xl">chessUI</Link>
+        <div className="navbar-end">
+          <ThemeSwitch />
+          {true ? 
+          <Link href={'/signup'} className="btn">Sign Up</Link>
+          : (
+            <div className="avatar avatar-placeholder">
+              <div className="bg-neutral text-neutral-content w-12 rounded-full">
+                <span className="text-xl">D</span>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li><a>Hire Me</a></li>
-          <li>
-            <details className="dropdown">
-              <summary>Mode</summary>
-              <ul className="menu dropdown-content bg-base-300 rounded-box z-1 w-52 p-2 shadow-sm">
-                <li><a>2 Player</a></li>
-                <li><a>vs Friend</a></li>
-                <li><a>Dice Chess</a></li>
-              </ul>
-            </details>
-          </li>
-          <li><a>Developer</a></li>
-        </ul>
-      </div>
-      <div className="navbar-end">
-        <ThemeSwitch />
-        <a className="btn">Sign Up / Login</a>
-      </div>
-    </div>
+      <Toaster />
+    </>
   );
 }
--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,6 +1,55 @@
+'use client'
+import { ResponseStatus } from "@/helper/response";
 import Link from "next/link";
+import { useRef } from "react";
+import toast from "react-hot-toast";
+
+interface SignUpFormType {
+  username: string;
+  email: string;
+  password: string;
+}
 
 export default function SignUp(){
+
+  const userData = useRef<SignUpFormType>({username:"",email:"",password:""});
+
+  const handleSignUp = async ({username,email,password}:SignUpFormType) => {
+
+    try {
+      const res = await fetch('/api/user/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({username,email,password}),
+      });
+  
+      const result = await res.json();
+      switch (result.status) {
+        case ResponseStatus.SUCCESS:
+          toast.success(result.message);
+          break;
+  
+        case ResponseStatus.ERROR:
+          toast.error(result.message);
+          break;
+  
+        case ResponseStatus.WARN:
+          toast.error(result.message);
+          break;
+  
+        default:
+          toast.error('Something went wrong');
+      }
+    } catch (error) {
+
+      console.error('Error:', error);
+      toast.error('Something went wrong');
+      
+    }
+  };
+
   return (
     <div className="flex w-screen h-screen items-center justify-center">
       <div>
@@ -8,15 +57,20 @@ export default function SignUp(){
           <legend className="fieldset-legend">Sign Up</legend>
 
           <label className="fieldset-label">Username</label>
-          <input type="text" className="input" placeholder="Username" />
+          <input type="text" className="input" placeholder="Username" onChange={e => userData.current.username = e.target.value} />
 
           <label className="fieldset-label">Email</label>
-          <input type="email" className="input" placeholder="Email" />
+          <input type="email" className="input" placeholder="Email" onChange={e => userData.current.email = e.target.value} />
 
           <label className="fieldset-label">Password</label>
-          <input type="password" className="input" placeholder="Password" />
+          <input type="password" className="input" placeholder="Password" onChange={e => userData.current.password = e.target.value} />
 
-          <button className="btn btn-neutral mt-4">Sign Up</button>
+          <button 
+            className="btn btn-neutral mt-4"
+            onClick={()=>{
+              handleSignUp(userData.current);
+            }}
+            >Sign Up</button>
           <p className="fieldset-label">Already have an account, <Link className="link" href={'/login'}>Login</Link></p>
         </fieldset>
       </div>
